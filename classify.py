318cb26fb897860fcd2fbe82cbcc94bfb43e200c
Mario54 <mariolamontagne54@gmail.com>
2017-11-26 10:39:28 -0500

    Refactor classify script

--- classify.py
+++ classify.py
@@ -34,7 +34,7 @@ def print_probabilities(class_labels, predict_proba):
             print("{}: {}%".format(c, round(p * 100, 2)))
 
 
-def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=None):
+def print_confusion_matrix(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=None):
     """pretty print for confusion matrixes"""
     columnwidth = max([len(x) for x in labels] + [5])  # 5 is value length
     empty_cell = " " * columnwidth
@@ -58,6 +58,42 @@ def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=
         print()
 
 
+class KNNClassifier():
+    def __init__(self):
+        self.knn = KNeighborsClassifier(n_neighbors=3, weights='distance')
+
+    def predict_one(self, training_data, classes, feature_vector):
+        self.knn.fit(training_data, classes)
+
+        return self.knn.predict_proba(feature_vector)[0]
+
+    def predict_all(self, x_train, x_test, y_train, y_test):
+        self.knn.fit(x_train, y_train)
+
+        return self.knn.predict(x_test)
+
+
+class GaussianClassifier():
+    def __init__(self):
+        self.gnb = GaussianNB()
+
+    def predict_one(self, training_data, classes, feature_vector):
+        self.gnb.fit(training_data, classes)
+
+        return self.gnb.predict_proba(feature_vector)[0]
+
+    def predict_all(self, x_train, x_test, y_train, y_test):
+        self.gnb.fit(x_train, y_train)
+
+        return self.gnb.predict(x_test)
+
+
+def normalized_confusion_matrix(y_test, predictions):
+    cm = confusion_matrix(y_test, predictions)
+    np.set_printoptions(precision=2)
+    return cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
 if __name__ == "__main__":
 
     parser = OptionParser()
@@ -65,7 +101,7 @@ if __name__ == "__main__":
                       help="classify this file's font")
     parser.add_option("--data", dest="data_file",
                       help="classify data file")
-    parser.add_option("--features-algo", dest="algo", default='gabor',
+    parser.add_option("--features-algo", dest="algo", default='text',
                       help="algorithm used to generate features")
     parser.add_option("--classifier", dest="classifier", default="knn")
 
@@ -77,102 +113,90 @@ if __name__ == "__main__":
         data_file = options.data_file
 
     all_data, classes = load_data(data_file)
-    if options.classifier == "knn":
 
-        knn = KNeighborsClassifier(n_neighbors=3, weights='distance')
+    classifiers = {
+        "knn": KNNClassifier(),
+        "gaussian": GaussianClassifier(),
+    }
+
+    if options.classifier in classifiers:
+
+        classifier = classifiers[options.classifier]
 
         if options.file is not None:
-            knn.fit(all_data, classes)
             feature_vector = np.asarray(extract_features_from_file(options.file, algo=options.algo)).reshape(1, -1)
-            print_probabilities(classes, knn.predict_proba(feature_vector)[0])
+            prediction = classifier.predict_one(all_data, classes, feature_vector)
+            print_probabilities(classes, prediction)
         else:
+            print("classifier: {}".format(options.classifier))
             x_train, x_test, y_train, y_test = train_test_split(all_data, classes, test_size=0.5)
-            knn.fit(x_train, y_train)
-            pred = knn.predict(x_test)
+            predictions = classifier.predict_all(x_train, x_test, y_train, y_test)
 
             print()
-            print(classification_report(y_test, pred))
-            print(confusion_matrix(y_test, pred))
+            print(classification_report(y_test, predictions))
+            print(confusion_matrix(y_test, predictions))
+            print(normalized_confusion_matrix(y_test, predictions))
 
-    elif options.classifier == "neural-net":
-        from sklearn.preprocessing import StandardScaler
 
-        scaler = StandardScaler()
-        clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                            )
+    else:
+        print("'{}' algorithm is not supported".format(options.classifier))
+        exit()
 
-        if options.file is not None:
-            scaler.fit(all_data)
-            all_data = scaler.transform(all_data)
-            clf.fit(all_data, classes)
-            feature_vector = np.asarray(extract_features_from_file(options.file, algo=options.algo)).reshape(1, -1)
-            print_probabilities(classes, clf.predict_proba(feature_vector)[0])
-        else:
-            x_train, x_test, y_train, y_test = train_test_split(all_data, classes, test_size=0.33)
+    # if options.classifier == "neural-net":
+    #     from sklearn.preprocessing import StandardScaler
+    #
+    #     scaler = StandardScaler()
+    #     clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+    #                         )
+    #
+    #     if options.file is not None:
+    #         scaler.fit(all_data)
+    #         all_data = scaler.transform(all_data)
+    #         clf.fit(all_data, classes)
+    #         feature_vector = np.asarray(extract_features_from_file(options.file, algo=options.algo)).reshape(1, -1)
+    #         print_probabilities(classes, clf.predict_proba(feature_vector)[0])
+    #     else:
+    #         x_train, x_test, y_train, y_test = train_test_split(all_data, classes, test_size=0.33)
+    #
+    #         # Don't cheat - fit only on training data
+    #         scaler.fit(x_train)
+    #         x_train = scaler.transform(x_train)
+    #         # apply same transformation to test data
+    #         x_test = scaler.transform(x_test)
+    #
+    #         clf.fit(x_train, y_train)
+    #         pred = clf.predict(x_test)
+    #
+    #         print(classification_report(y_test, pred))
+    #         cm = confusion_matrix(y_test, pred)
+    #
+    #         np.set_printoptions(precision=2)
+    #         normalized_cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    #
+    #         print(normalized_cm)
+    #         print_cm(cm, sorted(list(set(classes))))
+    #
+    # elif options.classifier == "svm":
+    #     svc = SVC(kernel='rbf', class_weight='balanced')
+    #
+    #     if options.file is not None:
+    #         svc.fit(all_data, classes)
+    #         feature_vector = np.asarray(extract_features_from_file(options.file, algo=options.algo)).reshape(1, -1)
+    #         print(svc.predict(feature_vector)[0])
+    #     else:
+    #         x_train, x_test, y_train, y_test = train_test_split(all_data, classes, test_size=0.33)
+    #
+    #         svc.fit(x_train, y_train)
+    #
+    #         pred = svc.predict(x_test)
+    #
+    #         print(classification_report(y_test, pred))
+    #         cm = confusion_matrix(y_test, pred)
+    #
+    #         np.set_printoptions(precision=2)
+    #         normalized_cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    #
+    #         print(normalized_cm)
+    #         print_cm(cm, sorted(list(set(classes))))
+    #
 
-            # Don't cheat - fit only on training data
-            scaler.fit(x_train)
-            x_train = scaler.transform(x_train)
-            # apply same transformation to test data
-            x_test = scaler.transform(x_test)
-
-            clf.fit(x_train, y_train)
-            pred = clf.predict(x_test)
-
-            print(classification_report(y_test, pred))
-            cm = confusion_matrix(y_test, pred)
-
-            np.set_printoptions(precision=2)
-            normalized_cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-            print(normalized_cm)
-            print_cm(cm, sorted(list(set(classes))))
-
-    elif options.classifier == "svm":
-        svc = SVC(kernel='rbf', class_weight='balanced')
-
-        if options.file is not None:
-            svc.fit(all_data, classes)
-            feature_vector = np.asarray(extract_features_from_file(options.file, algo=options.algo)).reshape(1, -1)
-            print( svc.predict(feature_vector)[0])
-        else:
-            x_train, x_test, y_train, y_test = train_test_split(all_data, classes, test_size=0.33)
-
-            svc.fit(x_train, y_train)
-
-            pred = svc.predict(x_test)
-
-            print(classification_report(y_test, pred))
-            cm = confusion_matrix(y_test, pred)
-
-            np.set_printoptions(precision=2)
-            normalized_cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-            print(normalized_cm)
-            print_cm(cm, sorted(list(set(classes))))
-
-    elif options.classifier == "gaussian":
-        gnb = GaussianNB()
-
-        if options.file is not None:
-            # print(classes)
-            gnb.fit(all_data, classes)
-            feature_vector = np.asarray(extract_features_from_file(options.file, algo=options.algo)).reshape(1, -1)
-            print(feature_vector)
-            print_probabilities(classes, gnb.predict_proba(feature_vector)[0])
-            print(gnb.theta_)
-            # print(gnb.sigma_)
-        else:
-            x_train, x_test, y_train, y_test = train_test_split(all_data, classes, test_size=0.33)
-            y_pred = gnb.fit(x_train, y_train)
-
-            pred = gnb.predict(x_test)
-
-            print(classification_report(y_test, pred))
-            cm = confusion_matrix(y_test, pred)
-
-            np.set_printoptions(precision=2)
-            normalized_cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-            print(normalized_cm)
-            # print_cm(cm, sorted(list(set(classes))))
